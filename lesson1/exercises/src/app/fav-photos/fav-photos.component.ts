2fbdb689e1bcb767f984ffb591681cb34a007a6d
Eydie <you@example.com>
2021-07-06 23:08:15 -0500

    done with lesson 1 exercise angular

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'LOOK AT THESE';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://images.pexels.com/photos/4065880/pexels-photo-4065880.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
   image3 = '';
 
   constructor() { }
---
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'page-title',
+  templateUrl: './page-title.component.html',
+  styleUrls: ['./page-title.component.css']
+})
+export class PageTitleComponent implements OnInit {
+  photoTitle = "All the photos are here!";
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  tit
+
+}
